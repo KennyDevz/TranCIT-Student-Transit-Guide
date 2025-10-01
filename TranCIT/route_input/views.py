2bf340ebf27c597fcf1965c0ce2816f0d01e0246
Dan Erik F. Dalapo <dalapodanerik@gmail.com>
2025-10-01 21:30:49 +0800

    feature(routes): added map

--- TranCIT/route_input/views.py
+++ TranCIT/route_input/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import folium
 
 def index(request):
     submitted_data = None
@@ -9,4 +10,16 @@ def index(request):
             'transport_type': request.POST.get('transport_type'),
             'fare': request.POST.get('fare'),
         }
-    return render(request, 'route_input/index.html', {'submitted_data': submitted_data})
+    
+    m = folium.Map(location=[10.3157, 123.8854], zoom_start=13)
+    folium.Marker([10.3157, 123.8854], popup="Cebu City").add_to(m)
+    map_html = m._repr_html_()
+    
+    return render(
+        request,
+        'route_input/index.html',
+        {
+            'submitted_data': submitted_data,
+            'map': map_html,
+        }
+    )
